5bb4376dbc70b25bd555df36d60e86065b690c32
jo <inizle35@gmail.com>
2024-06-27 17:35:29 +0900

    [uidesign]: ui design - 애니메이션 수정

--- app/src/main/java/pjo/travelapp/presentation/ui/fragment/SearchFragment.kt
+++ app/src/main/java/pjo/travelapp/presentation/ui/fragment/SearchFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import pjo.travelapp.R
-
+import pjo.travelapp.databinding.FragmentSearchBinding
+import pjo.travelapp.presentation.util.AppNavigator
+import pjo.travelapp.presentation.util.Fragments
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+    @Inject lateinit var navigator: AppNavigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +30,26 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        binding.ivBtnClose.setOnClickListener {
+            findNavController().navigateUp() // 현재 프래그먼트 종료
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
     companion object {
 
         @JvmStatic

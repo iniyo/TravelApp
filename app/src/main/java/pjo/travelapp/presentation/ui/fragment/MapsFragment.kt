b85aabfefb6c93267d452f95b2de70d881fbd409
DESKTOP-KR9167Q\inizl <inizle35@gmail.com>
2024-07-07 22:48:38 +0900

    []Ui_Design]: ui-design - splash screen 추가

--- app/src/main/java/pjo/travelapp/presentation/ui/fragment/MapsFragment.kt
+++ app/src/main/java/pjo/travelapp/presentation/ui/fragment/MapsFragment.kt
@@ -2,7 +2,6 @@ package pjo.travelapp.presentation.ui.fragment
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -42,20 +41,19 @@ class MapsFragment : Fragment() {
         googleMap = map
         val sydney = LatLng(32.557667, 126.926546)
         googleMap.apply {
-
             uiSettings.isZoomControlsEnabled = true
-            checkLocatePermissionAndEnableMyLocation()
             mapType = GoogleMap.MAP_TYPE_NORMAL
             moveCamera(CameraUpdateFactory.newLatLng(sydney))
             moveCamera(CameraUpdateFactory.zoomTo(15F))
         }
+        checkLocatePermissionAndEnableMyLocation()
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (isGranted) {
-            checkLocatePermissionAndEnableMyLocation()
+            enableMyLocation()
         }
     }
 
@@ -63,7 +61,7 @@ class MapsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMapsBinding.inflate(layoutInflater, container, false)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         return binding.root
@@ -89,17 +87,26 @@ class MapsFragment : Fragment() {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     val location = svMapsSearch.query.toString()
                     if (location.isNotEmpty()) {
-                        // Use Coroutine to perform geocoding in background
                         CoroutineScope(Dispatchers.IO).launch {
                             val geocoder = Geocoder(requireContext())
                             try {
-                                val addressList: List<Address>? = geocoder.getFromLocationName(location, 1)
+                                val addressList = withContext(Dispatchers.IO) {
+                                    geocoder.getFromLocationName(location, 1)
+                                }
                                 if (!addressList.isNullOrEmpty()) {
-                                    val address: Address = addressList[0]
+                                    val address = addressList[0]
                                     val latLng = LatLng(address.latitude, address.longitude)
                                     withContext(Dispatchers.Main) {
-                                        googleMap.addMarker(MarkerOptions().position(latLng).title("Marker in Busan"))
-                                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10F))
+                                        googleMap.addMarker(
+                                            MarkerOptions().position(latLng)
+                                                .title("Marker in $location")
+                                        )
+                                        googleMap.animateCamera(
+                                            CameraUpdateFactory.newLatLngZoom(
+                                                latLng,
+                                                10F
+                                            )
+                                        )
                                     }
                                 }
                             } catch (e: IOException) {
@@ -111,16 +118,15 @@ class MapsFragment : Fragment() {
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-                    // Implement this if necessary
                     return false
                 }
             })
         }
     }
 
+
     private fun setAnimation() {
         binding.apply {
-            // 처음 나타날 때 애니메이션 없이 숨겨진 상태로 설정
             svMapsSearch.visibility = View.INVISIBLE
             svMapsSearch.viewTreeObserver.addOnGlobalLayoutListener(object :
                 ViewTreeObserver.OnGlobalLayoutListener {
@@ -164,6 +170,22 @@ class MapsFragment : Fragment() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
+            enableMyLocation()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             googleMap.isMyLocationEnabled = true
             fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                 if (location != null) {
@@ -174,8 +196,6 @@ class MapsFragment : Fragment() {
                     googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
                 }
             }
-        } else {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 }
